634af0ca955a10ceb6e87cd31b1e8a35a8282b21
geomoetric <eric.g.moe@gmail.com>
2018-10-31 19:47:08 -0500

    feat(sketch.js): clickable

--- sketch.js
+++ sketch.js
@@ -1,5 +1,9 @@
-var x;
-var y;
+var x,
+y,
+w1
+w2,
+h1,
+h2;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -34,9 +38,31 @@ function rand() {
   this.y = random(height);
   this.diameter = random(width/9, width/5);
 
+  this.w1 = this.x - (this.diameter / 2);
+  this.w2 = this.x + (this.diameter / 2);
+  this.h1 = this.y - (this.diameter / 2);
+  this.h2 = this.y + (this.diameter / 2);
+
   this.display = function() {
     ellipse(this.x, this.y, this.diameter, this.diameter);
   };
-
 }
 
+function mousePressed() {
+  // if ( mouseX >= bounds.x && mouseX <= bounds.x + bounds.w &&
+  //var c = testo.diameter/2;
+  //console.log('diameter: ' + testo.diameter);
+  //var d = dist(mouseX, mouseY, c, c);
+  //console.log('distance: ' + d);
+
+  console.log('Width: ' + testo.w1 + '-' + testo.w2)
+  console.log('Height: ' + testo.h1 + '-' + testo.h2)
+  console.log('Mouse: x=' + mouseX + ', y=' + mouseY)
+
+  //if (d < testo.diameter) {
+  
+  if ( mouseX >= testo.w1 && mouseX <= testo.w2 && mouseY >= testo.h1 && mouseY <= testo.h2) {
+    alert('click');
+  }
+  // }
+}
