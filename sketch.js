636fb2d49dacd2ccf28e0edd945a56313acacef8
geomoetric <eric.g.moe@gmail.com>
2018-11-05 16:21:39 -0600

    feat(sketch): "like" selection is active

--- sketch.js
+++ sketch.js
@@ -1,9 +1,11 @@
 var x,
-y,
-w1
-w2,
-h1,
-h2;
+  y,
+  w1
+  w2,
+  h1,
+  h2,
+  sel,
+  liked;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -19,17 +21,18 @@ function windowResized() {
 }
 
 function draw() {
-  fill('#18181a')
+  fill('#18181a');
+  noStroke();
   ellipse(56, 46, 55, 55);
-  testo.display()
+  testo.display();
 
   downvote = createButton('&#128078;');
   downvote.position(width/2-30, height-40);
-  downvote.mousePressed(testo)
+  downvote.mousePressed(testo);
 
   upvote = createButton('&#128077;');
   upvote.position(width/2+30, height-40);
-  upvote.mousePressed(testo())
+  upvote.mousePressed(testo());
   //button.mousePressed(greet);
 }
 
@@ -37,32 +40,34 @@ function rand() {
   this.x = random(width);
   this.y = random(height);
   this.diameter = random(width/9, width/5);
+  sel = width/40;
 
   this.w1 = this.x - (this.diameter / 2);
   this.w2 = this.x + (this.diameter / 2);
   this.h1 = this.y - (this.diameter / 2);
   this.h2 = this.y + (this.diameter / 2);
 
+  this.highlight = function() {
+    fill('#40826d');
+    ellipse(this.x, this.y, this.diameter + sel, this.diameter + sel);
+  };
   this.display = function() {
+    fill('#18181a');
     ellipse(this.x, this.y, this.diameter, this.diameter);
   };
 }
 
 function mousePressed() {
-  // if ( mouseX >= bounds.x && mouseX <= bounds.x + bounds.w &&
-  //var c = testo.diameter/2;
-  //console.log('diameter: ' + testo.diameter);
-  //var d = dist(mouseX, mouseY, c, c);
-  //console.log('distance: ' + d);
 
-  console.log('Width: ' + testo.w1 + '-' + testo.w2)
-  console.log('Height: ' + testo.h1 + '-' + testo.h2)
-  console.log('Mouse: x=' + mouseX + ', y=' + mouseY)
+  //console.log('Width: ' + testo.w1 + '-' + testo.w2);
+  console.log('Height: ' + testo.h1 + '-' + testo.h2);
+  console.log('Mouse: x=' + mouseX + ', y=' + mouseY);
 
-  //if (d < testo.diameter) {
   
   if ( mouseX >= testo.w1 && mouseX <= testo.w2 && mouseY >= testo.h1 && mouseY <= testo.h2) {
-    alert('click');
+    //alert('click');
+    testo.highlight();
+    testo.display();
   }
   // }
 }
