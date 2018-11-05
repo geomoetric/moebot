4bd0f8498031db04d6a2422dbfb978ed734d70bf
geomoetric <eric.g.moe@gmail.com>
2018-11-05 16:52:58 -0600

    feat(sketch): select, deselect, and noselect
    - all three are working graphically
    - will need to update thier ability to send info

--- sketch.js
+++ sketch.js
@@ -25,6 +25,7 @@ function draw() {
   noStroke();
   ellipse(56, 46, 55, 55);
   testo.display();
+  sel = width/40;
 
   downvote = createButton('&#128078;');
   downvote.position(width/2-30, height-40);
@@ -40,17 +41,45 @@ function rand() {
   this.x = random(width);
   this.y = random(height);
   this.diameter = random(width/9, width/5);
-  sel = width/40;
+  this.liked = 0;
 
   this.w1 = this.x - (this.diameter / 2);
   this.w2 = this.x + (this.diameter / 2);
   this.h1 = this.y - (this.diameter / 2);
   this.h2 = this.y + (this.diameter / 2);
 
-  this.highlight = function() {
+  this.click = function() {
+
+    if (this.liked < 2) {
+      this.liked += 1;
+    } else {
+      this.liked = 0;
+    }
+
+    if (this.liked == 1) {
+      this.like();
+    } else if (this.liked == 2) {
+      this.dislike();
+    } else {
+      fill('#f8f8f9');
+      ellipse(this.x, this.y, this.diameter + sel + 10, this.diameter + sel + 10);
+      this.display();
+    }
+
+  }
+
+  this.like = function() {
     fill('#40826d');
     ellipse(this.x, this.y, this.diameter + sel, this.diameter + sel);
+    this.display();
   };
+
+  this.dislike = function() {
+    fill('#e32636');
+    ellipse(this.x, this.y, this.diameter + sel, this.diameter + sel);
+    this.display();
+  };
+
   this.display = function() {
     fill('#18181a');
     ellipse(this.x, this.y, this.diameter, this.diameter);
@@ -63,11 +92,10 @@ function mousePressed() {
   console.log('Height: ' + testo.h1 + '-' + testo.h2);
   console.log('Mouse: x=' + mouseX + ', y=' + mouseY);
 
-  
   if ( mouseX >= testo.w1 && mouseX <= testo.w2 && mouseY >= testo.h1 && mouseY <= testo.h2) {
     //alert('click');
-    testo.highlight();
-    testo.display();
+    testo.click();
+    //testo.display();
   }
   // }
 }
