8b8b5a19961b01ed8f83ebc49aada4f399f1ffc7
geomoetric <eric.g.moe@gmail.com>
2018-11-05 17:30:14 -0600

    feat(sketch): mock sql inserts
    - will change console logs to real insert statements later

--- sketch.js
+++ sketch.js
@@ -72,12 +72,14 @@ function rand() {
     fill('#40826d');
     ellipse(this.x, this.y, this.diameter + sel, this.diameter + sel);
     this.display();
+    console.log('INSERT INTO `table_name`(time, element, liked, x, y, w, h) VALUES (' + Date.now() + ', circle, TRUE, ' + this.x + ', ' + this.y + ', ' + this.diameter + ', ' + this.diameter + ');');
   };
 
   this.dislike = function() {
     fill('#e32636');
     ellipse(this.x, this.y, this.diameter + sel, this.diameter + sel);
     this.display();
+    console.log('INSERT INTO `table_name`(time, element, liked, x, y, w, h) VALUES (' + Date.now() + ', circle, FALSE, ' + this.x + ', ' + this.y + ', ' + this.diameter + ', ' + this.diameter + ');');
   };
 
   this.display = function() {
@@ -89,8 +91,8 @@ function rand() {
 function mousePressed() {
 
   //console.log('Width: ' + testo.w1 + '-' + testo.w2);
-  console.log('Height: ' + testo.h1 + '-' + testo.h2);
-  console.log('Mouse: x=' + mouseX + ', y=' + mouseY);
+  //console.log('Height: ' + testo.h1 + '-' + testo.h2);
+  //console.log('Mouse: x=' + mouseX + ', y=' + mouseY);
 
   if ( mouseX >= testo.w1 && mouseX <= testo.w2 && mouseY >= testo.h1 && mouseY <= testo.h2) {
     //alert('click');
